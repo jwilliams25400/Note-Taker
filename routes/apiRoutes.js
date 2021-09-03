9a0119f89ef4a04bb75b6daca479acdbbe9d563c
Jemel Williams <jwilliams25400@yahoo.com>
2021-09-03 17:32:06 -0400

    completed server js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,9 +2,8 @@ const path = require("path");
 const fs = require("fs");
 const router = require("express").Router();
 const { v4: uuidv4 } = require("uuid");
-const { notEqual, notStrictEqual } = require("assert");
 
-
+//set routes for api this gets notes saved in db.json
 router.get('/notes', (req, res) => {
     fs.readFile("../db/db.json", (err, data) => {
         if (err) throw err;
@@ -12,14 +11,16 @@ router.get('/notes', (req, res) => {
     })
 })
 
+// function post new notes to to db.json
 router.post('/notes', (req, res) => {
     console.log(req.body);
     // req.body.id = uuid();    
     fs.readFile("./db/db.json", (err, data) => {
         if (err) throw err;
-        req.body.id = uuid();
+        req.body.id = uuidv4();
         fs.writeFile("./db/db.json", JSON.stringify(json), (err) => {
             if (err) throw err;
+            console.log("Saved!");
             res.send(req.body)
 
         })
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const apiRoutes = require("./routes/apiRoutes.js");
+const htmlRoutes = require("./routes/htmlRoutes.js");
+
+const PORT =    process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Api server now on port ${PORT}`);

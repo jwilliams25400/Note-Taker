a9f18b2c226df2bd0a7df6e2a6c95f4ff8c6d756
Jemel Williams <jwilliams25400@yahoo.com>
2021-09-03 08:04:08 -0400

    completed apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,3 +2,28 @@ const path = require("path");
 const fs = require("fs");
 const router = require("express").Router();
 const { v4: uuidv4 } = require("uuid");
+const { notEqual, notStrictEqual } = require("assert");
+
+
+router.get('/notes', (req, res) => {
+    fs.readFile("../db/db.json", (err, data) => {
+        if (err) throw err;
+        res.status(201).send(data);
+    })
+})
+
+router.post('/notes', (req, res) => {
+    console.log(req.body);
+    // req.body.id = uuid();    
+    fs.readFile("./db/db.json", (err, data) => {
+        if (err) throw err;
+        req.body.id = uuid();
+        fs.writeFile("./db/db.json", JSON.stringify(json), (err) => {
+            if (err) throw err;
+            res.send(req.body)
+
+        })
+
+    })
+})
+module.exports = router

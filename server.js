e3d0946beec571c6bf3706ce44abc7f84f926e96
Jemel Williams <jwilliams25400@yahoo.com>
2021-09-03 17:35:17 -0400

    updated server js file

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const app = express();
 
 const apiRoutes = require("./routes/apiRoutes.js");
 const htmlRoutes = require("./routes/htmlRoutes.js");
 
 const PORT =    process.env.PORT || 3001;
-const app = express();
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -13,5 +14,6 @@ app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Api server now on port ${PORT}`);
+app.listen(PORT, () => 
+    console.log(`Api server now on port ${PORT} 🚀`)
+);
